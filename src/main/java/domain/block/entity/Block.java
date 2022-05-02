3b321624fa63683c3674602bed2f6020e000573a
고민혁 <rhalsgur@gmail.com>
2022-05-02 23:17:14 +0900

    create Blocks

--- src/main/java/domain/block/entity/Block.java
+++ src/main/java/domain/block/entity/Block.java
@@ -1,7 +1,60 @@
 package domain.block.entity;
 
+import domain.config.controller.BlockColorConfigController;
+
 public abstract class Block {
     
-    boolean isMovable;
-    boolean isRotatable;
+    protected int[][] shape;
+    protected int color;
+
+    protected boolean isRotatable;
+    protected boolean isMovable;
+
+    public Block() {
+
+    }
+
+    public Block(BlockColorConfigController colorSet) {
+        shape = new int[][] {
+            {0, 0},
+            {0, 0}
+        };
+
+        color = colorSet.getCurrentConfig().getiBlockColor();
+
+        isRotatable = true;
+        isMovable = true;
+    }
+
+    public int[][] getShape() {
+        return shape;
+    }
+
+    public int getColor() {
+        return color;
+    }
+
+    public void setShape(int[][] shape) {
+        this.shape = shape;
+    }
+
+    public void setColor(int color) {
+        this.color = color;
+    }
+
+    public boolean isMovable() {
+        return isMovable;
+    }
+
+    public boolean isRotatable() {
+        return isRotatable;
+    }
+
+    public void setMovable(boolean b) {
+        isMovable = b;
+    }
+
+    public void setRotatable(boolean b) {
+        isRotatable = b;
+    }
 }
