446331e8ffbb3420ca162dbe4fbb98ed9ebd0562
Taeuk Um <42173347+umtuk@users.noreply.github.com>
2022-05-10 12:54:55 +0900

    refactor: craete index frame with no test code

--- src/main/java/view/frame/IndexFrame.java
+++ src/main/java/view/frame/IndexFrame.java
@@ -1,8 +1,98 @@
 package view.frame;
 
+import java.awt.*;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
 import view.abstractComponent.frame.DefaultFrame;
+import view.keyListener.IndexKeyListener;
 
 public class IndexFrame extends DefaultFrame {
-    
 
+    private final String titleText = "테트리스";
+    private final String[] buttonTexts = {
+        "게임 시작",
+        "스코어 보드",
+        "환경 설정",
+        "나가기"
+    };
+
+    private final int COUNT = 4;
+    private int focusIndex;
+    
+    private JLabel title;
+    private JButton[] buttons;
+
+    public IndexFrame() {
+        BorderLayout bl = new BorderLayout();
+        GridLayout gl = new GridLayout(COUNT, 1);
+        setLayout(bl);
+        addKeyListener(new IndexKeyListener(this));
+
+        Font font = new Font("Noto sans", Font.BOLD, 30);
+        title = new JLabel(titleText);
+        title.setFont(font);
+        add(title, BorderLayout.NORTH);
+
+        buttons = new JButton[] {
+            new JButton(buttonTexts[0]),
+            new JButton(buttonTexts[1]),
+            new JButton(buttonTexts[2]),
+            new JButton(buttonTexts[3]),
+        };
+
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setLayout(gl);
+
+
+        for (int i = 0; i < COUNT; i++) {
+            decorateButton(buttons[i]);
+            buttonPanel.add(buttons[i]);
+        }
+        add(buttonPanel, BorderLayout.CENTER);
+
+        setFocusable(true);
+        decorateSelectedButton();
+        setVisible(true);
+    }
+
+    private void decorateButton(JButton button) {
+        button.setBackground(Color.BLACK);
+        button.setForeground(Color.WHITE);
+        button.setBorder(BorderFactory.createLineBorder(Color.WHITE));
+    }
+
+    public void shiftUpFocusIndex() {
+        decorateAllInNotFocus();
+        focusIndex = (focusIndex + COUNT - 1) % COUNT;
+        decorateSelectedButton();
+    }
+
+    public void shiftDownFocusIndex() {
+        decorateAllInNotFocus();
+        focusIndex = (focusIndex + 1) % COUNT;
+        decorateSelectedButton();
+    }
+
+    private void decorateAllInNotFocus() {
+        for (int i = 0; i < COUNT; i++)
+            decorateButton(buttons[i]);
+    }
+
+    private void decorateSelectedButton() {
+        buttons[focusIndex].setBackground(Color.WHITE);
+        buttons[focusIndex].setForeground(Color.BLACK);
+    }
+
+    public int getFocusIndex() {
+        return focusIndex;
+    }
+
+
+    public static void main(String[] args) {
+        new IndexFrame();
+    }
 }
--- src/main/java/view/keyListener/IndexKeyListener.java
+++ src/main/java/view/keyListener/IndexKeyListener.java
@@ -1,5 +1,38 @@
 package view.keyListener;
 
-public class IndexKeyListener {
+import java.awt.event.*;
+
+import view.frame.ConfigFrame;
+import view.frame.GameFrame;
+import view.frame.IndexFrame;
+import view.frame.ScoreBoardFrame;
+
+public class IndexKeyListener extends KeyAdapter {
     
+    private IndexFrame frame;
+
+    public IndexKeyListener(IndexFrame frame) {
+        this.frame = frame;
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        int keyCode = e.getKeyCode();
+
+        switch (keyCode) {
+            case KeyEvent.VK_UP: frame.shiftUpFocusIndex(); break;
+            case KeyEvent.VK_DOWN: frame.shiftDownFocusIndex(); break;
+            case KeyEvent.VK_ENTER: setConfig(); break;
+        }
+    }
+
+    public void setConfig() {
+
+        switch (frame.getFocusIndex()) {
+            case 0: frame.dispose(); new GameFrame(); break;
+            case 1: frame.dispose(); new ScoreBoardFrame(); break;
+            case 2: frame.dispose(); new ConfigFrame(); break;
+            case 3: frame.dispose(); break;
+        }
+    }
 }
