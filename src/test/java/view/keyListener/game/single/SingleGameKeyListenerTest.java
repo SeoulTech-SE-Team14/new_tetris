9be696c6bede8f0e83a60258a78815dca6840d0a
김영균 <kyoungyun98@naver.com>
2022-05-26 22:48:33 +0900

    비기능적 요구사항 테스트 추가

---
+++ src/test/java/view/keyListener/game/single/SingleGameKeyListenerTest.java
@@ -0,0 +1,69 @@
+package view.keyListener.game.single;
+
+import domain.block.entity.tetromino.TBlock;
+import domain.config.constant.key.KeyType;
+import domain.config.controller.KeyConfigController;
+import domain.config.entity.KeyConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import view.frame.game.GameFrame;
+
+import java.awt.event.KeyEvent;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SingleGameKeyListenerTest {
+    GameFrame gameFrame;
+    SingleGameKeyListener listener;
+    KeyConfig keyConfig = KeyConfigController.getInstance().getCurrentConfig();
+    @Test
+    @DisplayName("1초에 몇 번 움직임이 그려지는지 테스트")
+    void keyPressed() throws  InterruptedException {
+        gameFrame = new GameFrame();
+        listener = new SingleGameKeyListener(gameFrame);
+        gameFrame.addKeyListener(listener);
+        KeyEvent keyRight = new KeyEvent(gameFrame, KeyEvent.KEY_PRESSED, System
+                .currentTimeMillis(), 0, 54, 'a');
+        KeyEvent keyLeft = new KeyEvent(gameFrame, KeyEvent.KEY_PRESSED, System
+                .currentTimeMillis(), 0, 52, 'a');
+        KeyEvent keyDown = new KeyEvent(gameFrame, KeyEvent.KEY_PRESSED, System
+                .currentTimeMillis(), 0, 53, 'a');
+
+        gameFrame.getGamePanel().getBoardPanel().updateNowBlock(new TBlock());
+        int count = 0;
+        // 화면 중간으로 옮기기.
+        for(int i = 0; i < 5; i++) {
+            listener.keyPressed(keyDown);
+        }
+        // 블럭 초기 위치
+        int x = gameFrame.getGamePanel().getBoardPanel().getBoard().getCurX();
+        int y = gameFrame.getGamePanel().getBoardPanel().getBoard().getCurY();
+
+        long beforeTime = System.currentTimeMillis();
+        // 아래로 한 칸 내려가기 전까지
+        while (x == gameFrame.getGamePanel().getBoardPanel().getBoard().getCurX()) {
+            // 1초가 넘어가면 끝.
+            if((System.currentTimeMillis() - beforeTime) > 1000) break;
+            // 오른쪽으로 옮기기
+            if((count & 1) == 0){
+                listener.keyPressed(keyRight);
+                if(y + 1 != gameFrame.getGamePanel().getBoardPanel().getBoard().getCurY()) break;
+                //assertEquals(y + 1, gameFrame.getGamePanel().getBoardPanel().getBoard().getCurY());
+            }
+           else {
+                // 왼쪽으로 다시 옮기기
+                listener.keyPressed(keyLeft);
+                if(y != gameFrame.getGamePanel().getBoardPanel().getBoard().getCurY()) break;
+                //assertEquals(y, gameFrame.getGamePanel().getBoardPanel().getBoard().getCurY());
+            }
+            count++;
+            // 10ms 마다 한번 식 실행되도록 했음
+            // busy waiting이 발생 할지도 모르지만 일단 되니까..
+            TimeUnit.MILLISECONDS.sleep(10);
+        }
+        System.out.println(count);
+        assertTrue(count >= 5);
+    }
+
+}
